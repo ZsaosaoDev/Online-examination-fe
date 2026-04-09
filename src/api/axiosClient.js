94b509961a2ef5c9b446ef4d0cf96e5592d9340f
Nguyen Huy Vu <huyvu10012003@gmail.com>
2026-04-10 00:13:16 +0700

    fix

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,24 +1,25 @@
-import axios from "axios";
-
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8081/api/v1";
+import axios from 'axios';
+import API_URL from './api';
 
 const axiosClient = axios.create({
     baseURL: API_URL,
     headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
     },
-    withCredentials: true,  // send cookies when cross-domain requests
+    withCredentials: true,
 });
 
-
-axiosClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+axiosClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+);
 
 export default axiosClient;
